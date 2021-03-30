57cf26f17ee2a2d1f7a1258cf595c077a62a10b7
aman kumar <aman.fullstack.1192@gmail.com>
2021-03-30 23:30:44 +0530

    Added product page, router, homescreen and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import data from './data';
-import Product from './components/product'
+import {BrowserRouter, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
             <header className="row">
                 <div>
@@ -14,18 +16,14 @@ function App() {
                 </div>
             </header>
             <main>
-                <div className="row center">
-                {
-                  data.products.map(product => (
-                    <Product key ={product._id} product ={product}></Product>
-                  ))
-                }      
-                </div>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
             </main>
             <footer className="row center">
                 All Rights Reserved
             </footer>
         </div>
+    </BrowserRouter>
   );
 }
 
