40f675c4224d59d341f50a8dbed1752e5ade93a1
aman kumar <aman.fullstack.1192@gmail.com>
2021-04-04 13:15:03 +0530

    Added Loading box and Message box for error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import data from '../data';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import Product from '../components/product';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
+
 
 export default function HomeScreen(props) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () =>{
+      try {
+        setLoading(true);
+      const {data} = await axios.get('/api/products');
+      setLoading(false);
+      setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+      
+    };
+    fetchData();
+  }, [])
   return (
-        <div className="row center">
+    <div>
+    {loading ? (<LoadingBox></LoadingBox>)
+    :
+    error? (<MessageBox variant="danger">{error}</MessageBox>)
+    : (
+    <div className="row center">
                 {
-                  data.products.map(product => (
+                  products.map(product => (
                     <Product key ={product._id} product ={product}></Product>
                   ))
                 }      
         </div>
+    )}   
+    </div>
   );
 }
