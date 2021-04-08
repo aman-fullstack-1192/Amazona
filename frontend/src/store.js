f41362043188985d94a17e4a2fc17063738438a6
aman kumar <aman.fullstack.1192@gmail.com>
2021-04-08 23:56:47 +0530

    added product details page using redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,22 @@
 import data from "./data";
-import {applyMiddleware, createStore, compose, combineReducers} from 'redux';
-import thunk from 'redux-thunk'
-import { productListReducer } from "./reducers/productReducers";
+import { applyMiddleware, createStore, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialstate = {};
 const reducer = combineReducers({
-    productList: productListReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialstate, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  initialstate,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
